d61ba783e201c4702d2ad76fb801a32286e47aea
Jakub Szubart <szubiwtf@gmail.com>
2017-12-27 01:00:31 +0100

    Usuwanie Drużyn i nowa tabela z zawodnikami.

--- app/src/main/java/com/example/kuba/statmaster_app/DataBaseHelper.java
+++ app/src/main/java/com/example/kuba/statmaster_app/DataBaseHelper.java
@@ -2,6 +2,7 @@ package com.example.kuba.statmaster_app;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -14,16 +15,23 @@ import static com.example.kuba.statmaster_app.DrużynaStałe.Drużyny.TRENER;
 /**
  * Created by Kuba on 18.12.2017.
  */
+        //tworzenie bazy danych druzyn trzeba dodac id do tabeli z zawodnikami
 
         public class DataBaseHelper extends SQLiteOpenHelper {
         private static final String TAG = "DatabaseHelper";
         private static final String NAZWA_BAZY_DANYCH = "BazaDrużyn.db";
         private static final int WERSJA_BAZY_DANYCH = 2;
 
-        public static final String ID = "ID";
         public static final String NAZWA_TABELI = "BazaDrużyn";
+        public static final String ID = "ID";
         public static final String NAZWA_DRUŻYNY = "NAZWA";
         public static final String TRENER = "TRENER";
+        /*--------------------------------------------------*/
+        public static final String NAZWA_TABELI_ZAW = "BazaZawodników";
+        public static final String ID_ZAW = "ID_ZAW";
+        public static final String NAZWA_ZAW = "NAZWA_ZAW";
+        public static final String NUMER_ZAW = "NUMER_ZAW";
+
 
         public DataBaseHelper(Context context){
             super(context, NAZWA_BAZY_DANYCH, null, WERSJA_BAZY_DANYCH);
@@ -35,9 +43,10 @@ import static com.example.kuba.statmaster_app.DrużynaStałe.Drużyny.TRENER;
             NAZWA_DRUŻYNY + " TEXT NOT NULL," + TRENER + " TEXT NOT NULL);";
             bd.execSQL(createTable);
 
-
+            String createTableZaw = "CREATE TABLE " + NAZWA_TABELI_ZAW + "(" + ID_ZAW  + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            NAZWA_ZAW + " TEXT NOT NULL," + NUMER_ZAW + " INTEGER);";
+            bd.execSQL(createTableZaw);
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase bd, int staraWersja, int nowaWersja) {
             bd.execSQL("DROP TABLE IF EXISTS " + NAZWA_TABELI);
@@ -57,10 +66,30 @@ import static com.example.kuba.statmaster_app.DrużynaStałe.Drużyny.TRENER;
                 return false;
                 else
                     return true;
-
-
     }
 
+    public Cursor getTeams(){
+        SQLiteDatabase bd = this.getWritableDatabase();
+        String query = "SELECT * FROM " + NAZWA_TABELI;
+        Cursor data = bd.rawQuery(query, null);
+        return data;
+    }
+
+    public Cursor getTeamID(String nazwa){
+        SQLiteDatabase bd = this.getWritableDatabase();
+        String query = "SELECT " + ID + " FROM " + NAZWA_TABELI + " WHERE "
+                + NAZWA_DRUŻYNY  + " = '" + nazwa + "'";
+        Cursor data = bd.rawQuery(query, null);
+        return data;
+    }
+
+    public void deleteTeam (int id, String name){
+        SQLiteDatabase bd = this.getWritableDatabase();
+        String query = "DELETE FROM " + NAZWA_TABELI + " WHERE " + ID +
+                " = '" + id + "'" + " AND " + NAZWA_DRUŻYNY + " = '" + name + "'";
+        bd.execSQL(query);
+
+    }
 
 }
 
